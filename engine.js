a2e6fe3070c8ffa135d212f724779a47931b5e1d
Dario Seminara <dseminara@tbxnet.com>
2018-03-06 16:27:18 -0300

    simplificado a issue - type - subject

--- engine.js
+++ engine.js
@@ -50,78 +50,21 @@ module.exports = function (options) {
       // collection library if you prefer.
       cz.prompt([
         {
+          type: 'input',
+          name: 'issue',
+          message: 'Add issue reference number\n'
+        }, {
           type: 'list',
           name: 'type',
-          message: 'Select the type of change that you\'re committing:',
+          message: 'Select the type of change that you\'re committing:\n',
           choices: choices
         }, {
           type: 'input',
-          name: 'scope',
-          message: 'What is the scope of this change (e.g. component or file name)? (press enter to skip)\n'
-        }, {
-          type: 'input',
           name: 'subject',
-          message: 'Write a short, imperative tense description of the change:\n'
-        }, {
-          type: 'input',
-          name: 'body',
-          message: 'Provide a longer description of the change: (press enter to skip)\n'
-        }, {
-          type: 'confirm',
-          name: 'isBreaking',
-          message: 'Are there any breaking changes?',
-          default: false
-        }, {
-          type: 'input',
-          name: 'breaking',
-          message: 'Describe the breaking changes:\n',
-          when: function(answers) {
-            return answers.isBreaking;
-          }
-        }, {
-          type: 'confirm',
-          name: 'isIssueAffected',
-          message: 'Does this change affect any open issues?',
-          default: false
-        }, {
-          type: 'input',
-          name: 'issues',
-          message: 'Add issue references (e.g. "fix #123", "re #123".):\n',
-          when: function(answers) {
-            return answers.isIssueAffected;
-          }
+          message: 'Write imperative tense description of the change:\n'
         }
       ]).then(function(answers) {
-
-        var maxLineWidth = 100;
-
-        var wrapOptions = {
-          trim: true,
-          newline: '\n',
-          indent:'',
-          width: maxLineWidth
-        };
-
-        // parentheses are only needed when a scope is present
-        var scope = answers.scope.trim();
-        scope = scope ? '(' + answers.scope.trim() + ')' : '';
-
-        // Hard limit this line
-        var head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
-
-        // Wrap these lines at 100 characters
-        var body = wrap(answers.body, wrapOptions);
-
-        // Apply breaking change prefix, removing it if already present
-        var breaking = answers.breaking ? answers.breaking.trim() : '';
-        breaking = breaking ? 'BREAKING CHANGE: ' + breaking.replace(/^BREAKING CHANGE: /, '') : '';
-        breaking = wrap(breaking, wrapOptions);
-
-        var issues = answers.issues ? wrap(answers.issues, wrapOptions) : '';
-
-        var footer = filter([ breaking, issues ]).join('\n\n');
-
-        commit(head + '\n\n' + body + '\n\n' + footer);
+        commit('[#' + answers.issue + '] (' + answers.type + ') ' + answers.subject);
       });
     }
   };
